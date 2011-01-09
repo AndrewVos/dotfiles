d431857cdea5970fcea20600da7b7e10ff992498
AndrewVos <andrew.vos@gmail.com>
2011-01-09 09:37:44 +0000

    Made the installation a bit better. Remove and backups that were created from a previous installation

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
+echo "Deleting old backups..."
+rm -R ~/.vim.old
+rm ~/.vimrc.old
+
+echo "Backing up..."
 mv ~/.vim ~/.vim.old
 mv ~/.vimrc ~/.vimrc.old
 
+echo "Installing..."
 cp -R .vim ~/
 cp .vimrc ~/
