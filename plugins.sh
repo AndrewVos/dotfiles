62ec0d3a82e3d7e6a9fe278fb87a12ae223c10e8
Andrew Vos <andrew.vos@gmail.com>
2015-11-03 22:58:06 +0000

    Move plugins out to plugins.txt

--- plugins.sh
+++ plugins.sh
@@ -3,47 +3,10 @@
 set -u
 set -e
 
-plugins="tpope/vim-pathogen
-tpope/vim-sleuth
-tpope/vim-sensible
-tpope/vim-unimpaired
-tpope/vim-surround
-tpope/vim-commentary
-tpope/vim-fugitive
-tpope/vim-vinegar
-tpope/vim-jdaddy
-pangloss/vim-javascript
-jamessan/vim-gnupg
-kchmck/vim-coffee-script
-scrooloose/syntastic
-mattn/webapi-vim
-vim-scripts/Gist.vim
-AndrewVos/vim-aaa
-fatih/vim-go
-AndrewVos/dodo
-AndrewRadev/splitjoin.vim
-airblade/vim-gitgutter
-ekalinin/Dockerfile.vim
-sjl/gundo.vim
-tpope/vim-rails
-reedes/vim-wordy
-dietsche/vim-lastplace
-stefandtw/quickfix-reflector.vim
-AndrewVos/vim-git-navigator
-chriskempson/vim-tomorrow-theme
-godlygeek/tabular
-ntpeters/vim-better-whitespace
-junegunn/fzf
-tpope/vim-eunuch
-gorodinskiy/vim-coloresque
-tpope/vim-abolish
-int3/vim-extradite
-tpope/vim-obsession
-vim-ruby/vim-ruby
-"
+PLUGINS=$(cat plugins.txt)
 
 function install() {
-  for plugin in $plugins; do
+  for plugin in $PLUGINS; do
     dir=~/.vim/bundle/$(echo $plugin | cut -d "/" -f 2)
 
     if [ ! -d "$dir" ]; then
@@ -54,7 +17,7 @@ function install() {
 }
 
 function update() {
-  for plugin in $plugins; do
+  for plugin in $PLUGINS; do
     dir=~/.vim/bundle/$(echo $plugin | cut -d "/" -f 2)
 
     if [ -d "$dir" ]; then
@@ -68,7 +31,7 @@ function update() {
 function delete() {
   for plugin_directory in $(ls -d ~/.vim/bundle/*/); do
     should_be_installed="no"
-    for plugin in $plugins; do
+    for plugin in $PLUGINS; do
       expected_plugin_directory=~/.vim/bundle/$(echo $plugin | cut -d "/" -f 2)/
       if [ "$plugin_directory" == "$expected_plugin_directory" ]; then
         should_be_installed="yes"
@@ -88,13 +51,10 @@ if [ $# -eq 0 ]; then
 fi
 
 if [[ "$@" == *"--install"* ]]; then
-  echo "Installing all plugins..."
   install
 elif [[ "$@" == *"--update"* ]]; then
-  echo "Updating all plugins..."
   update
 elif [[ "$@" == *"--delete"* ]]; then
-  echo "Deleting old plugins..."
   delete
 fi
 
