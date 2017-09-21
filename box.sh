8739596a3c436b1fc630f7f8af5ac351874cf0d8
Andrew Vos <andrew.vos@gmail.com>
2017-09-21 20:12:47 +0100

    Add fonts-hack-ttf

--- box.sh
+++ box.sh
@@ -232,3 +232,7 @@ section "settings"
   # Screenshot with CMD+SHIFT+s
   satisfy dconf "/org/gnome/settings-daemon/plugins/media-keys/area-screenshot-clip" "<Shift><Super>s"
 end-section
+
+section "fonts"
+  satisfy apt "fonts-hack-ttf"
+end-section
