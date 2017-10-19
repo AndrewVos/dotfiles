e606c8a12fd4c0b03476621c85f4baad6a31804e
Andrew Vos <andrew.vos@gmail.com>
2017-10-19 20:12:59 +0100

    Caps Lock acts as Ctrl

--- box.sh
+++ box.sh
@@ -325,6 +325,7 @@ end-section
 section "settings"
   satisfy dconf "Take a screenshot with CMD+SHIFT+s" "org.gnome.settings-daemon.plugins.media-keys" "area-screenshot-clip" "<Shift><Super>s"
   satisfy dconf "Launch terminal with CMD+t" "org.gnome.settings-daemon.plugins.media-keys" "terminal" "<Super>t"
+  satisfy dconf "Caps Lock acts as Ctrl" "org.gnome.desktop.input-sources" "xkb-options" "['caps:ctrl_modifier']"
 end-section
 
 section "fonts"
