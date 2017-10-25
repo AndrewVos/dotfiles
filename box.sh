e7535270e43b65dcb8ef6839e4d8f395023f4528
Andrew Vos <andrew.vos@gmail.com>
2017-10-25 22:21:20 +0100

    Less hardcoded way of installing heroku

--- box.sh
+++ box.sh
@@ -251,7 +251,7 @@ section "cli tools"
     function install-heroku () {
       wget https://cli-assets.heroku.com/heroku-cli/channels/stable/heroku-cli-linux-x64.tar.gz -O heroku.tar.gz
       tar -xzf heroku.tar.gz
-      sudo mv heroku-cli-v6.14.35-e0fe5a3-linux-x64 /usr/local/lib/heroku
+      sudo mv heroku-cli-v6* /usr/local/lib/heroku
       sudo ln -s /usr/local/lib/heroku/bin/heroku /usr/local/bin/heroku
     }
     satisfy executable "heroku"
