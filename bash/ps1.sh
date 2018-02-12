c58f8e9289c9ce46636e6a47e46fdddc63fcf136
Andrew Vos <andrew.vos@gmail.com>
2018-02-12 13:43:50 +0000

    Add ctags config for javascript

--- bash/ps1.sh
+++ bash/ps1.sh
@@ -12,4 +12,4 @@ DATE_COLOUR="\033[1;30m"
 DIR_COLOUR="\033[1;34m"
 RESET_COLOUR="\033[0m"
 
-export PS1="\[$DATE_COLOUR\]\$(date +%H:%M:%S)\[$RESET_COLOUR\] \[$DIR_COLOUR\]\W\[$RESET_COLOUR\] \[\$(git_prompt_color)\]•\[$RESET_COLOUR\] "
+export PS1="\[$DATE_COLOUR\]\$(date +%H:%M:%S)\[$RESET_COLOUR\] \[$DIR_COLOUR\]\W\[$RESET_COLOUR\]\$(__git_ps1) \[\$(git_prompt_color)\]•\[$RESET_COLOUR\] "
