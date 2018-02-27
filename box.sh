c88337e08be085556989ee38ae8b07081d822e76
Andrew Vos <andrew.vos@gmail.com>
2018-02-27 11:59:10 +0000

    Add playerctl

--- box.sh
+++ box.sh
@@ -188,6 +188,7 @@ section "cli tools"
   satisfy apt "shellcheck"
   satisfy apt "tmate"
   satisfy apt "scrot"
+  satisfy deb "playerctl" "https://github.com/acrisci/playerctl/releases/download/v0.5.0/playerctl-0.5.0_amd64.deb"
 
   section "chromedriver"
     function install-chromedriver () {
