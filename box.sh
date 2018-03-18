ae7a9715047efa0b4d29537714c2154b3b7b7561
Andrew Vos <andrew.vos@gmail.com>
2018-03-18 22:17:07 +0000

    Remove fish shell

--- box.sh
+++ box.sh
@@ -210,7 +210,6 @@ section "dotfiles"
   mkdir -p "$HOME/.ssh"
   satisfy symlink "$DOTFILES_PATH/ssh/.ssh/config" "$HOME/.ssh/config"
 
-  satisfy symlink "$DOTFILES_PATH/fish" "$HOME/.config/fish"
   satisfy symlink "$DOTFILES_PATH/ctags/.ctags" "$HOME/.ctags"
 
   satisfy file-line "Add dotfiles scripts to PATH" ~/.bashrc "export PATH=\$PATH:$DOTFILES_PATH/scripts"
--- fish/config.fish
+++
@@ -1,4 +0,0 @@
-if status --is-interactive
-  set -g fish_user_abbreviations
-  abbr --add g "git"
-end
