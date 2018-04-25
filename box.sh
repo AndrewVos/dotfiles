61100bd3f146a665a6a0ed3bebd1237a6143f1df
Andrew Vos <andrew.vos@gmail.com>
2018-04-25 12:55:59 +0100

    Add eyedropper button

--- box.sh
+++ box.sh
@@ -115,6 +115,7 @@ section "apps"
   satisfy yaourt "xcalib"
   satisfy yaourt "herbstluftwm"
   satisfy yaourt "xrandr-invert-colors"
+  satisfy yaourt "colorpicker"
 
   satisfy yaourt "undistract-me-git"
   satisfy file-line "Source undistract-me" ~/.bashrc 'source /etc/profile.d/undistract-me.sh'
