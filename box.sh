31619533cc31ff3e1c7e9b4822bde68124418837
Andrew Vos <andrew.vos@gmail.com>
2018-05-29 18:08:13 +0100

    Add whois tool

--- box.sh
+++ box.sh
@@ -141,6 +141,7 @@ section "apps"
   satisfy yaourt "webtorrent-desktop"
   satisfy pacman "mpv"
   satisfy yaourt "rover"
+  satisfy pacman "whois"
 
   satisfy yaourt "undistract-me-git"
   satisfy file-line "Source undistract-me" ~/.bashrc 'source /etc/profile.d/undistract-me.sh'
