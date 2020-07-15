39983b056ab37f77e5784c2a6a6c21e84cbdca9f
Andrew Vos <andrew.vos@gmail.com>
2020-07-15 18:41:05 +0100

    Update spotify install

--- box.sh
+++ box.sh
@@ -114,8 +114,11 @@ section "apps"
   satisfy executable "slack"
 
   function install-spotify() {
-    sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 931FF8E79F0876134EDDBDCCA87FF9DF48BF1C90
-    echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
+    curl -sS https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add -
+    if [[ ! -f /etc/apt/sources.list.d/spotify.list ]]; then
+      echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+    fi
+
     sudo apt-get update -y
     sudo apt install -y spotify-client
   }
