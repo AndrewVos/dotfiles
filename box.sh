3e01de89b8e8ad3aba4e490b7ded789e9c762375
Andrew Vos <andrew.vos@gmail.com>
2020-07-15 14:31:53 +0100

    Remove zoom

--- box.sh
+++ box.sh
@@ -119,13 +119,6 @@ section "apps"
   }
   satisfy executable "slack"
 
-  function install-zoom() {
-    wget https://zoom.us/client/latest/zoom_amd64.deb -O zoom.deb
-    sudo apt install libxcb-xtest0
-    sudo dpkg -i zoom.deb
-  }
-  satisfy executable "zoom"
-
   function install-spotify() {
     sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 931FF8E79F0876134EDDBDCCA87FF9DF48BF1C90
     echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
