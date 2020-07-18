6e854737868cd70933b4d17cbe3cdce8336b3a26
Andrew Vos <andrew.vos@gmail.com>
2020-07-18 22:15:04 +0100

    Add dependencies for webtorrent

--- apps/webtorrent.sh
+++ apps/webtorrent.sh
@@ -6,6 +6,8 @@ if ! command -v "webtorrent-desktop" &> /dev/null; then
   sudo DEBIAN_FRONTEND=noninteractive apt install -y \
     keyboard-configuration \
     wget
+  sudo apt install -y libcanberra-gtk-module libcanberra-gtk3-module
+
 
   wget "https://github.com/webtorrent/webtorrent-desktop/releases/download/v0.23.0/webtorrent-desktop_0.23.0_amd64.deb" -O /tmp/webtorrent.deb
   sudo apt install -y /tmp/webtorrent.deb
