23b97acec4c22c974b2cc23d32938fefdedd0e25
Andrew Vos <andrew.vos@gmail.com>
2020-07-31 17:42:51 +0100

    Remove unused function

--- apps/cli/dotfiles.sh
+++ apps/cli/dotfiles.sh
@@ -6,19 +6,6 @@ sudo apt install -y \
   git \
   stow
 
-function git-clone() {
-  URL=$1
-  CLONE_PATH=$2
-
-  if [[ -d "$CLONE_PATH" ]]; then
-    if [[ "$UPGRADE" = "yes" ]]; then
-      git -C "$CLONE_PATH" pull
-    fi
-  else
-    git clone "$URL" "$CLONE_PATH"
-  fi
-}
-
 stow --verbose --dir "$HOME/.dotfiles" --target ~ bash ctags git ssh vim
 
 LINE="source $HOME/.dotfiles/bash/init.sh"
