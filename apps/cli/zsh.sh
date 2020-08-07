9f13a249db7f7c4e969d03173c7806076a14bca8
Andrew Vos <andrew.vos@gmail.com>
2020-08-07 15:44:02 +0100

    Set zsh as users default shell

--- apps/cli/zsh.sh
+++ apps/cli/zsh.sh
@@ -6,3 +6,7 @@ if ! command -v "zsh" &> /dev/null; then
   sudo apt update -y
   sudo apt install -y zsh
 fi
+
+if [[ "$SHELL" != "/usr/bin/zsh" ]]; then
+  sudo usermod --shell /usr/bin/zsh "$USER"
+fi
