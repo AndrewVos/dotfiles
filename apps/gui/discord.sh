ded3a9d10bb0905d648f580f9da89d673ce64250
Andrew Vos <andrew.vos@gmail.com>
2020-08-07 11:59:07 +0100

    Reinstall discord if version has changed

--- apps/gui/discord.sh
+++ apps/gui/discord.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-if ! command -v "discord" &> /dev/null; then
+function reinstall-discord() {
   sudo apt update -y
   sudo DEBIAN_FRONTEND=noninteractive apt install -y -q \
     keyboard-configuration \
@@ -10,4 +10,17 @@ if ! command -v "discord" &> /dev/null; then
 
   wget 'https://discordapp.com/api/download?platform=linux&format=deb' -O /tmp/discord.deb
   sudo apt install -y -q /tmp/discord.deb
+  LATEST_DISCORD_VERSION=$(curl --silent -I 'https://discordapp.com/api/download?platform=linux&format=deb'| grep 'location:' | cut -d ' ' -f 2)
+  echo "$LATEST_DISCORD_VERSION" > "$HOME/.installed-discord-version"
+}
+
+if [[ "$UPGRADE" = "yes" ]]; then
+  CURRENT_DISCORD_VERSION=$(cat "$HOME/.installed-discord-version" ||:)
+  LATEST_DISCORD_VERSION=$(curl --silent -I 'https://discordapp.com/api/download?platform=linux&format=deb'| grep 'location:' | cut -d ' ' -f 2)
+
+  if [[ "$CURRENT_DISCORD_VERSION" != "$LATEST_DISCORD_VERSION" ]]; then
+    reinstall-discord
+  fi
+elif ! command -v "discord" &> /dev/null; then
+  reinstall-discord
 fi
