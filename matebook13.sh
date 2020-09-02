d0ee382fc0192efceada2bf4f0a27f5e49da859a
Andrew Vos <andrew.vos@gmail.com>
2020-09-02 18:04:50 +0100

    Add machine-specific install scripts

---
+++ matebook13.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
+sudo pacman -S acpi acpilight
+
+sudo mkdir -p /etc/X11/xorg.conf.d
+
+sudo tee <<'EOF' /etc/X11/xorg.conf.d/90-touchpad.conf 1> /dev/null
+Section "InputClass"
+  Identifier "touchpad"
+  MatchIsTouchpad "on"
+  Driver "libinput"
+  Option "NaturalScrolling" "on"
+  Option "ScrollMethod" "twofinger"
+  Option "ClickMethod" "clickfinger"
+EndSection
+EOF
+
+sudo tee <<'EOF' /etc/X11/xorg.conf.d/00-keyboard.conf 1> /dev/null
+Section "InputClass"
+  Identifier "system-keyboard"
+  MatchIsKeyboard "on"
+  Option "XkbLayout" "gb"
+  Option "XkbModel" "pc104"
+  Option "XkbOptions" "ctrl:nocaps"
+EndSection
+EOF
+
+# /sys/class/backlight/amdgpu_bl0/brightness
+
+sudo tee <<'EOF' /etc/udev/rules.d/90-backlight.rules 1> /dev/null
+# Allow video group to control backlight and leds
+SUBSYSTEM=="backlight", ACTION=="add", \
+  RUN+="/bin/chgrp video /sys/class/backlight/amdgpu_bl0/brightness", \
+  RUN+="/bin/chmod g+w /sys/class/backlight/amdgpu_bl0/brightness"
+SUBSYSTEM=="leds", ACTION=="add", KERNEL=="*::kbd_backlight", \
+  RUN+="/bin/chgrp video /sys/class/leds/amdgpu_bl0/brightness", \
+  RUN+="/bin/chmod g+w /sys/class/leds/amdgpu_bl0/brightness"
+EOF
--- t480.sh
+++ t480.sh
@@ -2,10 +2,17 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-if laptop-detect; then
-  sudo mkdir -p /etc/X11/xorg.conf.d
+sudo mkdir -p /etc/X11/xorg.conf.d
 
-  sudo tee <<'EOF' /etc/X11/xorg.conf.d/90-touchpad.conf 1> /dev/null
+sudo tee <<'EOF' /etc/X11/xorg.conf.d/00-keyboard.conf 1> /dev/null
+Section "InputClass"
+  Identifier "system-keyboard"
+  MatchIsKeyboard "on"
+  Option "XkbOptions" "ctrl:nocaps"
+EndSection
+EOF
+
+sudo tee <<'EOF' /etc/X11/xorg.conf.d/90-touchpad.conf 1> /dev/null
 Section "InputClass"
   Identifier "touchpad"
   MatchIsTouchpad "on"
@@ -16,11 +23,10 @@ Section "InputClass"
 EndSection
 EOF
 
-    sudo tee <<'EOF' /etc/X11/xorg.conf.d/20-intel.conf 1> /dev/null
+sudo tee <<'EOF' /etc/X11/xorg.conf.d/20-intel.conf 1> /dev/null
 Section "Device"
   Identifier  "Intel Graphics"
   Driver      "intel"
   Option      "Backlight"  "intel_backlight"
 EndSection
 EOF
-fi
