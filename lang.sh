db6383269a54e7dd5bd50e8a542648b510b444b3
Andrew Vos <andrew.vos@gmail.com>
2020-09-14 16:30:55 +0100

    Install specific node

--- lang.sh
+++ lang.sh
@@ -5,5 +5,5 @@ IFS=$'\n\t'
 asdf plugin add ruby ||:
 asdf install ruby 2.7.1 ||:
 
-asdf plugin add nodejs ||:
+asdf plugin add nodejs 14.9.0 ||:
 NODEJS_CHECK_SIGNATURES=no asdf install nodejs latest ||:
