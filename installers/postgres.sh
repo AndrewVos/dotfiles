2b2c9451aa9c8bb612eb53da5d61d3e42e0eaa03
Andrew Vos <andrew.vos@gmail.com>
2021-04-06 12:33:14 +0100

    Set postgres timezone to UTC when installing

--- installers/postgres.sh
+++ installers/postgres.sh
@@ -6,7 +6,10 @@ ADDED_SUPERUSER_PATH="$HOME/.initialized-postgres"
 
 if [[ ! -f "$ADDED_SUPERUSER_PATH" ]]; then
   sudo su postgres -c "initdb --locale $LANG -E UTF8 -D /var/lib/postgres/data"
-  sudo systemctl enable --now postgresql
   sudo su postgres -c "createuser -s $USER"
   touch "$ADDED_SUPERUSER_PATH"
 fi
+
+echo "timezone='UTC'" | sudo tee -a /var/lib/postgres/data/postgresql.conf
+
+sudo systemctl enable --now postgresql
