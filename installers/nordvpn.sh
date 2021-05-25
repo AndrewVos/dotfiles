f9c72a7363c3e3cc55fb15352aceb197d9c07462
Andrew Vos <andrew.vos@gmail.com>
2021-05-25 09:06:16 +0100

    Add nordvpn to some group or whatever
    Otherwise it doesn't work.

--- installers/nordvpn.sh
+++ installers/nordvpn.sh
@@ -4,3 +4,7 @@ IFS=$'\n\t'
 
 sudo systemctl enable --now nordvpnd.service
 nordvpn whitelist add subnet 192.168.1.1/24 ||:
+
+if [[ ! $(groups $USER | grep nordvpn) ]]; then
+  sudo gpasswd -a $USER nordvpn
+fi
