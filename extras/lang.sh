e81e4f5e4e3cd5257eeacadb74ac3813e1f71a4f
Andrew Vos <andrew.vos@gmail.com>
2021-09-01 12:58:18 +0100

    Install eslint automatically

--- extras/lang.sh
+++ extras/lang.sh
@@ -3,10 +3,18 @@ set -euo pipefail
 IFS=$'\n\t'
 
 asdf plugin add ruby ||:
-asdf install ruby 2.7.1 ||:
+asdf install ruby 2.7.1
+asdf global ruby 2.7.1
 
 asdf plugin add nodejs ||:
 NODEJS_CHECK_SIGNATURES=no asdf install nodejs latest ||:
+asdf install nodejs 14.15.0
+asdf global nodejs 14.15.0
 
-asdf reshim
-yarn global add eslint
+if ! which yarn; then
+  npm install -g yarn
+fi
+
+if ! which eslint; then
+  yarn global add eslint
+fi
