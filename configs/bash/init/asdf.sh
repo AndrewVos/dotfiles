2b04ee2b4e50e85bc911173b96699a58ce9c274a
Andrew Vos <andrew.vos@gmail.com>
2021-09-20 14:36:26 +0100

    Remove bash config

--- configs/bash/init/asdf.sh
+++
@@ -1,4 +0,0 @@
-if [[ -f $HOME/.asdf/asdf.sh ]]; then
-  . $HOME/.asdf/asdf.sh
-  . $HOME/.asdf/completions/asdf.bash
-fi
--- configs/bash/init/golang.sh
+++
@@ -1,3 +0,0 @@
-export PATH="$PATH:/usr/local/go/bin"
-export GOPATH="$HOME/gopath"
-export PATH="$GOPATH/bin:$PATH"
