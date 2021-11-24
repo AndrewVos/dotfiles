6a900dc1aa170e9fa53838c3a39aae405b53bdbc
Andrew Vos <andrew.vos@gmail.com>
2021-11-24 11:02:30 +0000

    Launch ssh-agent

--- configs/fish/.config/fish/config.fish
+++ configs/fish/.config/fish/config.fish
@@ -57,3 +57,11 @@ set __fish_git_prompt_color_untrackedfiles red
 
 # asdf
 source /opt/asdf-vm/asdf.fish
+
+# ssh-agent
+if test -z (pgrep ssh-agent)
+  eval (ssh-agent -c)
+  set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
+  set -Ux SSH_AGENT_PID $SSH_AGENT_PID
+  set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
+end
