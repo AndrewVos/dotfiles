cc318e29293aebfdf6201897cdb77e21d8e60952
Andrew Vos <andrew.vos@gmail.com>
2022-04-25 16:14:18 +0100

    Remove default ruby/node installs

--- extras/lang.sh
+++ extras/lang.sh
@@ -3,19 +3,4 @@ set -euo pipefail
 IFS=$'\n\t'
 
 asdf plugin add ruby ||:
-asdf install ruby 2.7.1
-asdf global ruby 2.7.1
-
 asdf plugin add nodejs ||:
-asdf install nodejs 16.14.0 ||:
-asdf global nodejs 16.14.0 ||:
-
-asdf reshim
-
-if ! which yarn; then
-  npm install -g yarn
-fi
-
-if ! which eslint; then
-  yarn global add eslint
-fi
