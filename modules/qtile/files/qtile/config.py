d40d015824949ba2582b038b9143da30522e54ba
Andrew Vos <andrew.vos@gmail.com>
2022-05-19 23:43:19 +0100

    Bring in my bswpm theme

--- modules/qtile/files/qtile/config.py
+++ modules/qtile/files/qtile/config.py
@@ -109,19 +109,13 @@ for i in groups:
     )
 
 layouts = [
-    layout.Columns(border_focus_stack=["#d75f5f", "#8f3d3d"], border_width=4),
-    layout.Max(),
-    # Try more layouts by unleashing below layouts.
-    # layout.Stack(num_stacks=2),
-    # layout.Bsp(),
-    # layout.Matrix(),
-    # layout.MonadTall(),
-    # layout.MonadWide(),
-    # layout.RatioTile(),
-    # layout.Tile(),
-    # layout.TreeTab(),
-    # layout.VerticalTile(),
-    # layout.Zoomy(),
+  layout.MonadTall(
+    border_width = 5,
+    border_focus = '#61afef',
+    border_normal = '#1E2127',
+    margin = 10,
+    ),
+  layout.Max()
 ]
 
 widget_defaults = dict(
