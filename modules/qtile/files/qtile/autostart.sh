899c05fc2ef98d681237a896b7400904a958a204
Andrew Vos <andrew.vos@gmail.com>
2022-05-19 23:41:38 +0100

    Autostart some apps

---
+++ modules/qtile/files/qtile/autostart.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
+if laptop-detect; then
+  xrdb -merge "$HOME/.Xresources.laptop"
+fi
+
+picom &
+redshift -l $(curl -s "https://location.services.mozilla.com/v1/geolocate?key=geoclue" | jq -r '"\(.location.lat):\(.location.lng)"') &
+xmousepasteblock &
+hsetroot -solid '#F7FAFC' &
+xsetroot -cursor_name left_ptr &
+unclutter --timeout 1 &
+
+if [[ $(hostnamectl hostname) = "desktop" ]]; then
+  # stop autofocusing the camera
+  v4l2-ctl \
+    --media-bus-info usb-0000:00:14.0-2 \
+    --device "HD Pro Webcam C920" \
+    --set-ctrl focus_auto=0 &
+
+  # delay shutting down the monitor
+  xset s 3600 3600 &
+  xset -dpms &
+fi
--- modules/qtile/files/qtile/config.py
+++ modules/qtile/files/qtile/config.py
@@ -28,9 +28,17 @@ from libqtile import bar, layout, widget
 from libqtile.config import Click, Drag, Group, Key, Match, Screen
 from libqtile.lazy import lazy
 from libqtile.utils import guess_terminal
+from libqtile import hook
+import os
+import subprocess
 
 mod = "mod4"
 terminal = guess_terminal()
+
+@hook.subscribe.startup_once
+def autostart():
+    home = os.path.expanduser('~/.config/qtile/autostart.sh')
+    subprocess.run([home])
 
 keys = [
     # A list of available commands that can be bound to keys can be found
