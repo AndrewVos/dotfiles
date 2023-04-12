061cbbe906e45541fcc6b7f3a7b6c238fc0c5aa6
Andrew Vos <andrew.vos@gmail.com>
2023-04-12 20:31:09 +0100

    Remove unused scripts

--- install-scripts.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-IFS=$'\n\t'
-source ./helpers.sh
-
-if [[ "$SHELL" = "/bin/bash" ]]; then
-    add-line-to-file "$HOME/.bashrc" 'export PATH="$HOME/.dotfiles/scripts:$PATH"'
-fi
-PATH="$HOME/.dotfiles/scripts:$PATH"
