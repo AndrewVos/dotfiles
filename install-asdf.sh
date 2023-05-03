31088c26017cc998c88ce494a326e42b2faf20d7
Andrew Vos <andrew.vos@quidco.com>
2023-05-03 12:03:30 +0100

    Add osx packages and config

--- install-asdf.sh
+++ install-asdf.sh
@@ -12,6 +12,9 @@ if [[ "$SHELL" = "/bin/bash" ]]; then
     add-line-to-file "$HOME/.bashrc" '. "$HOME/.asdf/completions/asdf.bash"'
     . "$HOME/.asdf/asdf.sh"
     . "$HOME/.asdf/completions/asdf.bash"
+elif [[ "$SHELL" = "/bin/zsh" ]]; then
+    add-line-to-file "$HOME/.zshrc" '. "$HOME/.asdf/asdf.sh"'
+    . "$HOME/.asdf/asdf.sh"
 fi
 
 asdf plugin list | grep --quiet nodejs || asdf plugin add nodejs
