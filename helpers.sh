51c3e293d48624e986f8459adeb5da8b49f0e38b
Andrew Vos <andrew.vos@gmail.com>
2023-05-04 22:02:36 +0100

    Move osx helpers to helpers.sh

--- helpers.sh
+++ helpers.sh
@@ -73,3 +73,28 @@ function sudo-add-line-to-file() {
 
     file-has-line "$FILE" "$LINE" || echo "$LINE" | sudo tee -a "$FILE"
 }
+
+function brew-installed() {
+    PACKAGE="$1"
+    brew list "$PACKAGE" >/dev/null 2>&1
+}
+
+function brew-tapped() {
+    TAP="$1"
+    brew tap | grep -qxF "$TAP"
+}
+
+function brew-tap() {
+    TAP="$1"
+    brew-tapped "$TAP" || brew tap "$TAP"
+}
+
+function brew-install() {
+    PACKAGE="$1"
+    brew-installed "$PACKAGE" || brew install "$PACKAGE"
+}
+
+function brew-install-cask() {
+    PACKAGE="$1"
+    brew-installed "$PACKAGE" || brew install --cask "$PACKAGE"
+}
--- install-osx-packages.sh
+++ install-osx-packages.sh
@@ -1,31 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 IFS=$'\n\t'
-
-function brew-installed() {
-    PACKAGE="$1"
-    brew list "$PACKAGE" >/dev/null 2>&1
-}
-
-function brew-tapped() {
-    TAP="$1"
-    brew tap | grep -qxF "$TAP"
-}
-
-function brew-tap() {
-    TAP="$1"
-    brew-tapped "$TAP" || brew tap "$TAP"
-}
-
-function brew-install() {
-    PACKAGE="$1"
-    brew-installed "$PACKAGE" || brew install "$PACKAGE"
-}
-
-function brew-install-cask() {
-    PACKAGE="$1"
-    brew-installed "$PACKAGE" || brew install --cask "$PACKAGE"
-}
+source ./helpers.sh
 
 brew-install stow
 brew-install wget
