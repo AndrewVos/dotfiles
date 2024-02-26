7282a12a249c4a0ba30c5be3347729d773abe274
Andrew Vos <andrew.vos@gmail.com>
2024-02-26 11:45:19 +0000

    Add ruby to default install

--- install-asdf.sh
+++ install-asdf.sh
@@ -20,3 +20,24 @@ fi
 asdf plugin list | grep --quiet nodejs || asdf plugin add nodejs
 NODEJS_VERSION="19.8.1"
 asdf list nodejs | grep --quiet "$NODEJS_VERSION" || asdf install nodejs "$NODEJS_VERSION"
+
+# ruby requirements
+apt-install autoconf
+apt-install patch
+apt-install build-essential
+apt-install rustc
+apt-install libssl-dev
+apt-install libyaml-dev
+apt-install libreadline6-dev
+apt-install zlib1g-dev
+apt-install libgmp-dev
+apt-install libncurses5-dev
+apt-install libffi-dev
+apt-install libgdbm6
+apt-install libgdbm-dev
+apt-install libdb-dev
+apt-install uuid-dev
+
+asdf plugin list | grep --quiet ruby || asdf plugin add ruby
+RUBY_VERSION="3.3.0"
+asdf list ruby | grep --quiet "$RUBY_VERSION" || asdf install ruby "$RUBY_VERSION"
