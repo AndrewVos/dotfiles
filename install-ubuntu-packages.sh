79da563fc12457a2c4a67e59fd921ec34ea787b9
Andrew Vos <andrew.vos@gmail.com>
2024-05-01 09:52:01 +0100

    Fix broken enpass

--- install-ubuntu-packages.sh
+++ install-ubuntu-packages.sh
@@ -9,4 +9,12 @@ apt-install stow
 apt-install fonts-hack-ttf
 apt-install gnome-tweaks
 snap-install-classic code
-snap-install enpass
+
+if ! apt-installed "enpass"; then
+    sudo-add-line-to-file "/etc/apt/sources.list.d/enpass.list" "deb https://apt.enpass.io/ stable main"
+    if [[ ! -f " /etc/apt/trusted.gpg.d/enpass.asc" ]]; then
+        wget -O - https://apt.enpass.io/keys/enpass-linux.key | sudo tee /etc/apt/trusted.gpg.d/enpass.asc
+    fi
+    sudo apt update -y
+    apt-install enpass
+fi
